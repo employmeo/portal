8ecc8bdae56541cfb1b7240739bbaf979252a36f
Sri <sridharkaza@yahoo.com>
2017-09-17 08:17:16 -0700

    allow portal user to add new grader or wave min grade requirements

---
+++ src/main/java/com/talytica/portal/objects/NewGraderRequest.java
@@ -0,0 +1,21 @@
+package com.talytica.portal.objects;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.ToString;
+
+@Data
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+public class NewGraderRequest {
+
+	@NonNull String firstName;
+	String lastName;
+	@NonNull String email;
+	@NonNull Long questionId;
+	@NonNull Long respondantId;
+
+}
